0c773433857f75f366a4cada7ab3b24a86e23160
anand dudi <dudis.vision@gmail.com>
2024-03-15 16:30:54 +0530

    some improvement

--- main.js
+++ main.js
@@ -1,46 +1,50 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
-
-
+import * as THREE from "https://cdn.skypack.dev/three@0.135.0";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,10,1000)
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  10,
+  1000
+);
 
-const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setPixelRatio(window.devicePixelRatio)
-
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
 
 const geomatry = new THREE.SphereGeometry(10);
-const material = new THREE.MeshStandardMaterial({color: 0x00ff00});
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("./earthtexture.jpg");
+const material = new THREE.MeshStandardMaterial({
+  map: texture,
+});
 material.roughness = 0;
-material.wireframe = true
 
+const sphere = new THREE.Mesh(geomatry, material);
+scene.add(sphere);
 
-const sphere = new THREE.Mesh(geomatry,material);
-scene.add(sphere)
+const light = new THREE.DirectionalLight(0xfaf8fe, 1);
+scene.add(light);
 
-const light = new THREE.DirectionalLight( 0xFAF8FE ,1);
-scene.add(light)
+light.position.set(1, 0, 0.8);
 
-light.position.set(1,0,0.8)
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-renderer.setSize(window.innerWidth,window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-camera.position.z = 40
-function animate(){
+camera.position.z = 40;
+function animate() {
   window.requestAnimationFrame(animate);
-  renderer.render(scene,camera)
-  sphere.rotation.y += 1
-  sphere.rotation.x += -0.01
+  renderer.render(scene, camera);
+  sphere.rotation.y += 0.01;
+  sphere.rotation.x += -0.01;
   // light.rotation.y += 0.01
   // camera.rotation.y += 0.01
 }
 
-animate()
+animate();
 
-window.addEventListener('mousemove',(event)=>{
-  // light.position.y = -(event.clientY * 0.01)
-  // light.position.x = event.clientX*0.01
-  // light.position.z = event.clientY * 0.01
-})
-
+window.addEventListener("mousemove", (event) => {
+  console.log("here");
+  // sphere.rotation.z = event.clientY * 0.01;
+  sphere.rotation.y = event.clientX * 0.01;
+  sphere.rotation.x = event.clientY * 0.01;
+});
